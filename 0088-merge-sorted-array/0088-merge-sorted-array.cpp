3b2c5c34f1a6743cc2bef1872f3c5b904c283046
Harshal Savaliya <88780621+hetx19@users.noreply.github.com>
2025-09-21 17:52:30 +0530

    Time: 0 ms (100%), Space: 12.3 MB (70.54%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,31 +1,20 @@
 class Solution {
-private:
-    void swapIfGreater(vector<int>& nums1, int ind1, vector<int>& nums2, int ind2) {
-        if(nums1[ind1] > nums2[ind2]) {
-            swap(nums1[ind1], nums2[ind2]);
-        }
-    }
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int len = m + n;
-        int gap = (len/2) + (len % 2);
+        int left = m -1, right = 0;
 
-        while(gap > 0) {
-            int left = 0, right = left + gap;
-            while(right < len) {
-                if(left < m && right >= m) {
-                    swapIfGreater(nums1, left, nums2, right - m);
-                } else if(left >= m && right >= m) {
-                    swapIfGreater(nums2, left - m, nums2, right - m);
-                } else {
-                    swapIfGreater(nums1, left, nums1, right);
-                }
-                left++;
+        while(left >= 0 && right < n) {
+            if(nums1[left] > nums2[right]) {
+                swap(nums1[left], nums2[right]);
+                left--;
                 right++;
+            } else {
+                break;
             }
-            if(gap == 1) break;
-            gap = (gap / 2) + (gap % 2);
         }
+
+        sort(nums1.begin(), nums1.begin() + m);
+        sort(nums2.begin(), nums2.end());
 
         // Copy sorted nums2 into nums1
         for (int i = 0; i < n; i++) {
