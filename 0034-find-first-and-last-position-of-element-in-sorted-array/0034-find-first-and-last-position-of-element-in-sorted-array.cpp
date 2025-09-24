06a8e0611ae7dac981c47c72bbf74ae5d546d93a
Harshal Savaliya <88780621+hetx19@users.noreply.github.com>
2025-09-24 11:14:57 +0530

    Time: 0 ms (100%), Space: 17.4 MB (86.44%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,52 +1,48 @@
 class Solution {
 public:
-    int firstOccurrence(vector<int>& nums, int target) {
+    int lowerBound(vector<int>& nums, int target) {
         int low = 0, high = nums.size() - 1;
-        int first = -1;
+        int ans = nums.size();
 
         while(low <= high) {
             int mid = low + (high - low)/2;
-            if(nums[mid] == target) {
-                first = mid;
+            if(nums[mid] >= target) {
+                ans = mid;
                 high = mid - 1;
-            } else if(nums[mid] > target) {
-                high = mid - 1;
-            } else {
+            }   else {
                 low = mid + 1;
             }
         }
 
-        return first;
+        return ans;
     }
 
-    int lastOccurence(vector<int>& nums, int target) {
+    int upperBound(vector<int>& nums, int target) {
         int low = 0, high = nums.size() - 1;
-        int last = -1;
+        int ans = nums.size();
 
         while(low <= high) {
             int mid = low + (high - low)/2;
-            if(nums[mid] == target) {
-                last = mid;
-                low = mid + 1;
-            } else if(nums[mid] > target) {
+            if(nums[mid] > target) {
+                ans = mid;
                 high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
 
-        return last;
+        return ans;
     }
 
     vector<int> searchRange(vector<int>& nums, int target) {
-        // By using Raw Binary Search
-        int first = firstOccurrence(nums, target);
+        // By using lower_bound and upper_bound functions
+        int first = lowerBound(nums, target);
 
-        if(first == -1) {
+        if(first == nums.size() || nums[first] != target) {
             return {-1, -1};
         }
 
-        int last = lastOccurence(nums, target);
+        int last = upperBound(nums, target) - 1;
 
         return {first, last};
     }
