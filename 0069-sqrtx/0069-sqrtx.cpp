fea63fdb1eb0b7bb5ffb88b8ab84ea519d2d8dbf
Harshal Savaliya <88780621+hetx19@users.noreply.github.com>
2025-09-30 11:11:45 +0530

    Time: 0 ms (100%), Space: 8.5 MB (50.46%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0 || x == 1) {
+            return x;
+        }
+        
+        int low = 1, high = x;
+        int ans = 1;
+        
+        while(low <= high) {
+            long long int mid = low + ((high - low) >> 1);
+            long long int sq_value = pow(mid, 2);
+            
+            if(sq_value <= x) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        return ans;
+    }
+};
