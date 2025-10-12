7fd54e9b10a664b082921285e3907c2d4ba274ec
Harshal Savaliya <88780621+hetx19@users.noreply.github.com>
2025-10-12 10:40:36 +0530

    Time: 0 ms (100%), Space: 26 MB (37.5%) - LeetHub

--- 4068-sum-of-elements-with-frequency-divisible-by-k/4068-sum-of-elements-with-frequency-divisible-by-k.cpp
+++ 4068-sum-of-elements-with-frequency-divisible-by-k/4068-sum-of-elements-with-frequency-divisible-by-k.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int sumDivisibleByK(vector<int>& nums, int k) {
         int sum = 0, n = nums.size();
-        unordered_map<int, int> numFreqMap;
+        int freq[101] = {0};
 
         // Pre storing frequencies in a map data structure
-        for(int i = 0; i < n; i++) {
-            numFreqMap[nums[i]]++;
+        for(int x : nums) {
+            freq[x]++;
         }
 
-        for(auto it : numFreqMap) {
-            if(it.second % k == 0) {
-                sum += (it.first * it.second);
+        for(int i = 0; i < 100; i++) {
+            if(freq[i + 1] % k == 0) {
+                sum += ((i + 1) * freq[i + 1]);
             }
         }
 
