5b6c9b320dace3958e2a2000bd05720fa2baf2f2
Harshal Savaliya <88780621+hetx19@users.noreply.github.com>
2025-10-26 09:19:51 +0530

    Time: 15 ms (43.63%), Space: 123.7 MB (30.79%) - LeetHub

--- 2169-simple-bank-system/2169-simple-bank-system.cpp
+++ 2169-simple-bank-system/2169-simple-bank-system.cpp
@@ -1,34 +1,40 @@
 class Bank {
-private:
-    vector<long long> balance;
-
 public:
-    Bank(vector<long long>& balance) : balance(balance) {}
+    vector<long long> bal;
+
+    bool isValidAc(int account){
+        return account >= 1 && account <= bal.size();
+    }
+
+    Bank(vector<long long>& balance) {
+       bal = balance;
+    }
     
     bool transfer(int account1, int account2, long long money) {
-        if (account1 > balance.size() || account2 > balance.size() ||
-            balance[account1 - 1] < money) {
-            return false;
+        if(isValidAc(account1) && isValidAc(account2)) {
+            if(bal[account1-1] >= money){
+                bal[account2 - 1] = bal[account2 - 1] + money;
+                bal[account1 - 1] = bal[account1 - 1] - money;
+                return true;
+            }
         }
-        balance[account1 - 1] -= money;
-        balance[account2 - 1] += money;
-        return true;
+        return false;
     }
     
     bool deposit(int account, long long money) {
-        if(account > balance.size()) {
-            return false;
+        if(isValidAc(account)) {
+            bal[account-1] += money;
+            return true;
         }
-        balance[account - 1] += money;
-        return true;
+        return false;
     }
     
     bool withdraw(int account, long long money) {
-        if(account > balance.size() || balance[account - 1] < money) {
-            return false;
+        if(isValidAc(account) && bal[account-1] >= money) {
+            bal[account-1] -= money;
+            return true;
         }
-        balance[account - 1] -= money;
-        return true;
+        return false;
     }
 };
 
