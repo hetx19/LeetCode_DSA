45be53cd218c22e575a3d0e5245327f3fb848175
Harshal Savaliya <88780621+hetx19@users.noreply.github.com>
2025-11-07 17:56:36 +0530

    Time: 12 ms (64.2%), Space: 17.4 MB (67.12%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -14,20 +14,12 @@ public:
             return root;
         }
 
-        if(root == p) {
-            return p;
-        }
-
-        if(root == q) {
-            return q;
-        }
-
         TreeNode* left = lowestCommonAncestor(root->left, p, q);
         TreeNode* right = lowestCommonAncestor(root->right, p, q);
 
         if (left == NULL && right == NULL) {
             return NULL;
-        }else if(left == NULL) {
+        } else if(left == NULL) {
             return right;
         } else if(right == NULL) {
             return left;
