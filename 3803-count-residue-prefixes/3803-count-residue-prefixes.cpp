83c3275adecc07fa6f9ada02119d783f880bf5b4
Harshal Savaliya <88780621+hetx19@users.noreply.github.com>
2026-01-12 07:52:57 +0530

    Time: 0 ms (100%), Space: 10.1 MB (9.09%) - LeetHub

--- 3803-count-residue-prefixes/3803-count-residue-prefixes.cpp
+++ 3803-count-residue-prefixes/3803-count-residue-prefixes.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     int residuePrefixes(string s) {
         int n = s.size(), ans = 0; 
-        unordered_set<int> st;
+        unordered_set<char> st;
 
         for (int i = 0; i < n; i++) {
             st.insert(s[i]);
+            if (st.size() >= 3) {
+                break;
+            }
             if (st.size() == (i + 1) % 3) {
                 ans++;
             }
-            if (st.size() >= 3) return ans;
         }
 
         return ans;
