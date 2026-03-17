a9c8263640dad89ca252377b0f32a5fff72cd8f1
Harshal Savaliya <88780621+hetx19@users.noreply.github.com>
2026-03-17 08:37:39 +0530

    Time: 3 ms (13.5%), Space: 18.1 MB (55.71%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,28 +1,34 @@
 class Solution {
-private:
-    void dfs(int row, int column, vector<vector<int>>& ans, vector<vector<int>>& image, int initialColor, int newColor, int deltaRow[], int deltaCol[]) {
-        ans[row][column] = newColor;
-        int n = image.size(), m = image[0].size();
-
-        for (int i = 0; i < 4; i++) {
-            int nrow = row + deltaRow[i];
-            int ncol = column + deltaCol[i];
-
-            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && image[nrow][ncol] == initialColor && ans[nrow][ncol] != newColor) {
-                dfs(nrow, ncol, ans, image, initialColor, newColor, deltaRow, deltaCol);
-            }
-        }
-    }
-
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int m = image.size(), n = image[0].size();
+
         int initialColor = image[sr][sc];
         vector<vector<int>> ans = image;
 
         int deltaRow[] = {-1, 0, +1, 0};
         int deltaCol[] = {0, +1, 0, -1};
 
-        dfs(sr, sc, ans, image, initialColor, color, deltaRow, deltaCol);
+        queue<pair<int, int>> q;
+        q.push({sr, sc});
+
+        while (!q.empty()) {
+            int row = q.front().first;
+            int col = q.front().second;
+
+            ans[row][col] = color;
+
+            q.pop();
+
+            for (int i = 0; i < 4; i++) {
+                int nrow = row + deltaRow[i];
+                int ncol = col + deltaCol[i];
+
+                if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && image[nrow][ncol] == initialColor && ans[nrow][ncol] != color) {
+                    q.push({nrow, ncol});
+                }
+            }
+        }
 
         return ans;
     }
