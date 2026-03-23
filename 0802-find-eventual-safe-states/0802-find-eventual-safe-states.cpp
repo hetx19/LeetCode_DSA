1444fd6be9af7e9d61aeb4072ff7b8d7c678081e
Harshal Savaliya <88780621+hetx19@users.noreply.github.com>
2026-03-23 19:00:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,42 +1,49 @@
 class Solution {
+private:
+    bool dfsCheck(vector<vector<int>> &adj, vector<bool> &visited, vector<bool> &pathVisited, int node, vector<bool> &check) {
+	    visited[node] = true;
+	    pathVisited[node] = true;
+	    check[node] = false;
+	
+	    for (int it : adj[node]) {
+		    if (!visited[it]) {
+			    if (dfsCheck(adj, visited, pathVisited, it, check)) {
+                    check[node] = false;
+				    return true;
+			    }
+		    } else if (pathVisited[it]) {
+			    check[node] = false;
+			    return true;
+		    }
+	    }
+	
+	    check[node] = true;
+	    pathVisited[node] = false;
+	    return false;
+    }
+
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int V = graph.size();
-	    vector<vector<int>> adj(V);
-	    vector<int> indegree(V, 0);
-	
-	    for (int i = 0; i < V; i++) {
-		    for (int it : graph[i]) {
-			    adj[it].push_back(i);
-			    indegree[i]++;
-		    }
-	    }
-	
-	    queue<int> q;
-	    for (int i = 0; i < V; i++) {
-		    if (indegree[i] == 0) {
-			    q.push(i);
-		    }
-	    }
-	
-	    vector<int> ans;
-	
-	    while (!q.empty()) {
-		    int node = q.front();
-		    q.pop();
-		
-		    ans.push_back(node);
-		
-		    for (int it : adj[node]) {
-			    indegree[it]--;
-			    if (indegree[it] == 0) {
-				    q.push(it);
-			    }
-		    }
-	    }
-
-        sort(ans.begin(), ans.end());
-	
-	    return ans;
+	    int V = graph.size();
+    
+        vector<bool> visited(V, false);
+        vector<bool> pathVisited(V, false);
+        vector<bool> check(V, false);
+    
+        vector<int> ans;
+    
+        for (int i = 0; i < V; i++) {
+	        if (!visited[i]) {
+		        dfsCheck(graph, visited, pathVisited, i, check);
+	        }
+        }
+    
+        for (int i = 0; i < V; i++) {
+	        if (check[i]) {
+		        ans.push_back(i);
+	        }
+        }
+    
+        return ans;
     }
 };
