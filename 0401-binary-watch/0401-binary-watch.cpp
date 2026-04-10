9de48748367c8de0721ce769e2fa0bbc5c124149
Harshal Savaliya <88780621+hetx19@users.noreply.github.com>
2026-04-10 08:16:30 +0530

    Time: 0 ms (100%), Space: 9.6 MB (58.82%) - LeetHub

---
+++ 0401-binary-watch/0401-binary-watch.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
+        vector<string> ans;
+
+        for (int hour = 0; hour < 12; hour++) {
+            for (int min = 0; min < 60; min++) {
+                int cnt = __builtin_popcount(hour) + __builtin_popcount(min);
+
+                if (cnt == turnedOn) {
+                    string time = to_string(hour) + ":";
+
+                    if (min < 10) {
+                        time += "0";
+                    }
+
+                    time += to_string(min);
+
+                    ans.push_back(time);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
