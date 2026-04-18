4fe43c2fa6cb20344c22e56f7c6c5ce2faa09fcd
Harshal Savaliya <88780621+hetx19@users.noreply.github.com>
2026-04-18 08:33:52 +0530

    Time: 7 ms (9.82%), Space: 10.4 MB (45.78%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0, y = 0;
+
+        for (char move : moves) {
+            if (move == 'U') {
+                y++;
+            } else if (move == 'D') {
+                y--;
+            } else if (move == 'R') {
+                x++;
+            } else {
+                x--;
+            }
+        }
+
+        return (x == 0 && y == 0) ? true : false;
+    }
+};
