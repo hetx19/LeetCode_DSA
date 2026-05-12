6458132deeab9ba3de3128a202547842e05e1b46
Harshal Savaliya <88780621+hetx19@users.noreply.github.com>
2026-05-12 13:01:58 +0530

    Time: 290 ms (76.63%), Space: 167.1 MB (87.5%) - LeetHub

---
+++ 3927-minimize-array-sum-using-divisible-replacements/3927-minimize-array-sum-using-divisible-replacements.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    long long minArraySum(vector<int>& nums) {
+        const int MAXV = 100000;
+        vector<bool> exists(MAXV + 1, false);
+
+        for (int x : nums) {
+            exists[x] = true;
+        }
+
+        long long ans = 0;
+
+        for (int x : nums) {
+            int best = x;
+
+            for (int d = 1; 1LL * d * d <= x; d++) {
+                if (x % d == 0) {
+                    if (exists[d]) {
+                        best = d;
+                        break;
+                    }
+
+                    int other = x / d;
+
+                    if (exists[other]) {
+                        best = min(best, other);
+                    }
+                }
+            }
+
+            ans += best;
+        }
+
+        return ans;
+    }
+};
