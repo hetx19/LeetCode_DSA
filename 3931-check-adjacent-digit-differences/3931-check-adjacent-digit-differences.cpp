674d58119e6d61f2a3e751f2247e4d4b24a29170
Harshal Savaliya <88780621+hetx19@users.noreply.github.com>
2026-05-17 13:06:37 +0530

    Time: 0 ms (100%), Space: 8.7 MB (48.35%) - LeetHub

---
+++ 3931-check-adjacent-digit-differences/3931-check-adjacent-digit-differences.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isAdjacentDiffAtMostTwo(string s) {
+        int n = s.size();
+
+        for (int i = 0; i < n - 1; i++) {
+            if (abs((s[i + 1] - '0') - (s[i] - '0')) > 2) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
