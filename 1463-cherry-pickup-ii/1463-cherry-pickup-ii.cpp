198e4531c95312255f84f721a4e8d9bae2a9d7d3
Harshal Savaliya <88780621+hetx19@users.noreply.github.com>
2026-07-13 09:56:46 +0530

    Time: 244 ms (5%), Space: 67.7 MB (5.03%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,50 +1,52 @@
 class Solution {
-  private:
-	int solve(vector<vector<int>>& grid, int row, int col1, int col2, int n, int m, vector<vector<vector<int>>>& dp) {
-		if (col1 < 0 || col1 >= m || col2 < 0 || col2 >= m) {
-			return INT_MIN;
-		}
-		
-		if (row == n - 1) {
-			if (col1 == col2) {
-				return dp[row][col1][col2] = grid[row][col1];
-			}
-			
-			return dp[row][col1][col2] = grid[row][col1] + grid[row][col2];
-		}
-
-        if (dp[row][col1][col2] != -1) {
-            return dp[row][col1][col2];
-        }
-		
-		int maxi = INT_MIN;
-		vector<int> deltaCol = {-1, 0, +1};
-		
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				int value = 0;
-				if (col1 == col2) {
-					value = grid[row][col1];
-				} else {
-					value = grid[row][col1] + grid[row][col2];
-				}
-				
-				int ncol1 = col1 + deltaCol[i];
-				int ncol2 = col2 + deltaCol[j];
-				value += solve(grid, row + 1, ncol1, ncol2, n, m, dp);
-				
-				maxi = max(maxi, value);
-			}
-		}
-		
-		return dp[row][col1][col2] = maxi;
-	}
-	
   public:
     int cherryPickup(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>> (m, vector<int>(m, -1)));
-
-        return solve(grid, 0, 0, m - 1, n, m, dp);
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int> (m, 0)));
+        
+        for (int col1 = 0; col1 < m; col1++) {
+	        for (int col2 = 0; col2 < m; col2++) {
+		        if (col1 == col2) {
+			        dp[n - 1][col1][col2] = grid[n - 1][col1];
+		        } else {
+			        dp[n - 1][col1][col2] = grid[n - 1][col1] + grid[n - 1][col2];
+		        }
+	        }
+        }
+        
+        for (int i = n - 2; i >= 0; i--) {
+	        for (int col1 = 0; col1 < m; col1++) {
+		        for (int col2 = 0; col2 < m; col2++) {
+			        int maxi = INT_MIN;
+			        vector<int> deltaCol = {-1, 0, +1};
+			        
+					for (int x = 0; x < 3; x++) {
+						for (int y = 0; y < 3; y++) {
+							int value = 0;
+							if (col1 == col2) {
+								value = grid[i][col1];
+							} else {
+								value = grid[i][col1] + grid[i][col2];
+							}
+				
+							int ncol1 = col1 + deltaCol[x];
+							int ncol2 = col2 + deltaCol[y];
+							
+							if (ncol1 >= 0 && ncol1 < m && ncol2 >= 0 && ncol2 < m) {
+								value += dp[i + 1][ncol1][ncol2];
+							} else {
+								value += INT_MIN;
+							}
+							
+							maxi = max(maxi, value);
+						}
+					}
+					
+					dp[i][col1][col2] = maxi;
+		        }
+	        }
+        }
+        
+        return dp[0][0][m - 1];
     }
 };
