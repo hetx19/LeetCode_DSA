188dc611ba6f69f7783b9d6ea4d4547d7f0c0bae
Harshal Savaliya <88780621+hetx19@users.noreply.github.com>
2026-07-22 07:22:46 +0530

    Time: 0 ms (100%), Space: 12.9 MB (62.39%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -2,28 +2,29 @@ class Solution {
   private:
     int perfectSum(vector<int>& arr, int target) {
         int n = arr.size();
-        vector<vector<int>> dp(n, vector<int>(target + 1, 0));
+        vector<int> prev(target + 1, 0), current(target + 1, 0);
 
-        dp[0][0] = (arr[0] == 0) ? 2 : 1;
+        prev[0] = (arr[0] == 0) ? 2 : 1;
 
         if (arr[0] != 0 && arr[0] <= target) {
-            dp[0][arr[0]] = 1;
+            prev[arr[0]] = 1;
         }
 
         for (int index = 1; index < n; index++) {
             for (int t = 0; t <= target; t++) {
-                int notPick = dp[index - 1][t];
+                int notPick = prev[t];
 
                 int pick = 0;
                 if (arr[index] <= t) {
-                    pick = dp[index - 1][t - arr[index]];
+                    pick = prev[t - arr[index]];
                 }
 
-                dp[index][t] = pick + notPick;
+                current[t] = pick + notPick;
             }
+            prev = current;
         }
 
-        return dp[n - 1][target];
+        return prev[target];
     }
     
     int countPartitions(vector<int>& arr, int diff) {
