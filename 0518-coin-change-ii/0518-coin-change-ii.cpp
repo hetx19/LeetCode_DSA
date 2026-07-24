0f38a2a419884c830305be0b2c87516eecca1339
Harshal Savaliya <88780621+hetx19@users.noreply.github.com>
2026-07-24 17:48:51 +0530

    Time: 35 ms (41.38%), Space: 36.2 MB (67.19%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,25 +1,26 @@
 class Solution {
-  private:
-	int solve(vector<int>& coins, int index, int amount, vector<vector<int>>& dp) {
-		if (index == 0) {
-			return dp[index][amount] = ((amount % coins[index] == 0) ? 1 : 0);
-		}
-		
-		if (dp[index][amount] != -1) {
-			return dp[index][amount];
-		}
-		
-		int notTake = solve(coins, index - 1, amount, dp);
-		int take = (coins[index] <= amount) ? solve(coins, index, amount - coins[index], dp) : 0;
-		
-		return dp[index][amount] = notTake + take;
-	}
-	
   public:
 	int change(int amount, vector<int>& coins) {
 		int n = coins.size();
-		vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+		vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
 		
-		return solve(coins, n - 1, amount, dp);
+		for (int target = 0; target <= amount; target++) {
+			dp[0][target] = (target % coins[0] == 0);
+		}
+		
+		for (int index = 1; index < n; index++) {
+			for (int target = 0; target <= amount; target++) {
+				long long notTake = dp[index - 1][target];
+				long long take = 0;
+                
+                if (coins[index] <= target) {
+                    take = dp[index][target - coins[index]];
+                }
+				
+				dp[index][target] = take + notTake;
+			}
+		}
+		
+		return dp[n - 1][amount];
 	}
 };
