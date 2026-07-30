d7ca563d21bfde7bb2d0a9144b2ab03c4781931f
Harshal Savaliya <88780621+hetx19@users.noreply.github.com>
2026-07-30 06:54:28 +0530

    Time: 0 ms (100%), Space: 8.3 MB (77.73%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int minimumPushes(string word) {
+    int minimumPushes(string& word) {
         int fullGroups = word.size() >> 3, remainingChars = word.size() & 7;
 
         return ((fullGroups << 2) + remainingChars) * (fullGroups + 1);
