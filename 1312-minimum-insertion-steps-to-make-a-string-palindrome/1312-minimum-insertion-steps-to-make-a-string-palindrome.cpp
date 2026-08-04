698890af99ca414705fb416ab9f45d88eee55305
Harshal Savaliya <88780621+hetx19@users.noreply.github.com>
2026-08-04 07:49:54 +0530

    Time: 12 ms (97.38%), Space: 9.8 MB (89.79%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -2,27 +2,24 @@ class Solution {
   private:
 	int longestCommonSubsequence(string text1, string text2) {
 		int n1 = text1.size(), n2 = text2.size();
-		vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+		vector<int> prev(n2 + 1, 0), current(n2 + 1, 0);
 		
 		for (int index2 = 0; index2 <= n2; index2++) {
-			dp[0][index2] = 0;
-		}
-		
-		for (int index1 = 0; index1 <= n1; index1++) {
-			dp[index1][0] = 0;
+			prev[index2] = 0;
 		}
 		
 		for (int index1 = 1; index1 <= n1; index1++) {
 			for (int index2 = 1; index2 <= n2; index2++) {
 				if (text1[index1 - 1] == text2[index2 - 1]) {
-					dp[index1][index2] = 1 + dp[index1 - 1][index2 - 1];
+					current[index2] = 1 + prev[index2 - 1];
 				} else {
-					dp[index1][index2] = max(dp[index1 - 1][index2], dp[index1][index2 - 1]);
+					current[index2] = max(prev[index2], current[index2 - 1]);
 				}
 			}
+			prev = current;
 		}
 		
-		return dp[n1][n2];
+		return current[n2];
 	}
 	
 	int longestPalindromeSubseq(string s) {
