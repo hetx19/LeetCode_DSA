1bd01b1d99ff062eacfe821cd562950cb0cbefe7
Harshal Savaliya <88780621+hetx19@users.noreply.github.com>
2026-08-05 08:25:17 +0530

    Time: 19 ms (13.09%), Space: 17.4 MB (7.8%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,26 +1,20 @@
 class Solution {
 private:
-    int solve(string word1, int index1, string word2, int index2, vector<vector<int>>& dp) {
-        if (index1 == 0 || index2 == 0) {
-            return dp[index1][index2] = 0;
-        }
-
-        if (dp[index1][index2] != -1) {
-            return dp[index1][index2];
-        }
-
-        if (word1[index1 - 1] == word2[index2 - 1]) {
-            return dp[index1][index2] = 1 + solve(word1, index1 - 1, word2, index2 - 1, dp);
-        }
-
-        return dp[index1][index2] = max(solve(word1, index1 - 1, word2, index2, dp), solve(word1, index1, word2, index2 - 1, dp));
-    }
-
     int lcs(string word1, string word2) {
         int n1 = word1.size(), n2 = word2.size();
-        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, -1));
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
 
-        return solve(word1, n1, word2, n2, dp);
+        for (int index1 = 1; index1 <= n1; index1++) {
+            for (int index2 = 1; index2 <= n2; index2++) {
+                if (word1[index1 - 1] == word2[index2 - 1]) {
+                    dp[index1][index2] = 1 + dp[index1 - 1][index2 - 1];
+                } else {
+                    dp[index1][index2] = max(dp[index1 - 1][index2], dp[index1][index2 - 1]);
+                }
+            }
+        }
+
+        return dp[n1][n2];
     }
 
 public:
