7d41feb861641ffcf996edde1d0d44162ed33dc6
Harshal Savaliya <88780621+hetx19@users.noreply.github.com>
2026-08-07 05:51:10 +0530

    Time: 15 ms (82.3%), Space: 8.9 MB (91.54%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -2,22 +2,21 @@ class Solution {
   public:
 	int numDistinct(string s, string t) {
 		int n1 = s.size(), n2 = t.size();
-		vector<vector<double>> dp(n1 + 1, vector<double>(n2 + 1, 0));
+		vector<double> prev(n2 + 1, 0), current(n2 + 1, 0);
 		
-		for (int index1 = 0; index1 <= n1; index1++) {
-			dp[index1][0] = 1;
-		}
+		prev[0] = current[0] = 1;
 		
 		for (int index1 = 1; index1 <= n1; index1++) {
 			for (int index2 = 1; index2 <= n2; index2++) {
 				if (s[index1 - 1] == t[index2 - 1]) {
-					dp[index1][index2] = dp[index1 - 1][index2 - 1] + dp[index1 - 1][index2];
+					current[index2] = prev[index2 - 1] + prev[index2];
 				} else {
-					dp[index1][index2] = dp[index1 - 1][index2];
+					current[index2] = prev[index2];
 				}
 			}
+			prev = current;
 		}
 		
-		return (int)(dp[n1][n2]);
+		return (int)(prev[n2]);
 	}
 };
