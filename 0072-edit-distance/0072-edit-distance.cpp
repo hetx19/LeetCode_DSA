9c5eb9c90ce6570ba3e9c9a4a6ea0fa97ea0194b
Harshal Savaliya <88780621+hetx19@users.noreply.github.com>
2026-08-07 19:05:16 +0530

    Time: 6 ms (68.49%), Space: 10.3 MB (98.88%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -2,26 +2,24 @@ class Solution {
   public:
 	int minDistance(string word1, string word2) {
 		int n1 = word1.size(), n2 = word2.size();
-		vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
-		
-		for (int index1 = 0; index1 <= n1; index1++) {
-			dp[index1][0] = index1;
-		}
+		vector<int> prev(n2 + 1, 0), current(n2 + 1, 0);
 		
 		for (int index2 = 0; index2 <= n2; index2++) {
-			dp[0][index2] = index2;
+			prev[index2] = index2;
 		}
 		
 		for (int index1 = 1; index1 <= n1; index1++) {
+			current[0] = index1;
 			for (int index2 = 1; index2 <= n2; index2++) {
 				if (word1[index1 - 1] == word2[index2 - 1]) {
-					dp[index1][index2] = dp[index1 - 1][index2 - 1];
+					current[index2] = prev[index2 - 1];
 				} else {
-					dp[index1][index2] = 1 + min({dp[index1 - 1][index2 - 1], dp[index1][index2 - 1], dp[index1 - 1][index2]});
+					current[index2] = 1 + min({prev[index2 - 1], current[index2 - 1], prev[index2]});
 				}
 			}
+			prev = current;
 		}
 		
-		return dp[n1][n2];
+		return prev[n2];
 	}
 };
