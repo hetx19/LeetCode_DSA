c0136d1508b10b7fcd53777dafbf57b97e5e65d2
Harshal Savaliya <88780621+hetx19@users.noreply.github.com>
2026-08-16 09:25:13 +0530

    Time: 27 ms (41.32%), Space: 177.5 MB (84%) - LeetHub

--- 3693-climbing-stairs-ii/3693-climbing-stairs-ii.cpp
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.cpp
@@ -1,30 +1,22 @@
 class Solution {
-private:
-    int solve(vector<int>& costs, int n, int index, vector<int>& dp) {
-        if (index == n) {
-            return dp[index] = 0;
-        }
-
-        if (dp[index] != -1) {
-            return dp[index];
-        }
-
-        int cost = INT_MAX;
-
-        for (int i = 0; i < 3; i++) {
-            int next = index + i + 1;
-
-            if (next <= n) {
-                cost = min(cost, solve(costs, n, next, dp) + costs[next - 1] + (i + 1) * (i + 1));
-            }
-        }
-
-        return dp[index] = cost;
-    }
-
 public:
     int climbStairs(int n, vector<int>& costs) {
-        vector<int> dp(n + 1, -1);
-        return solve(costs, n, 0, dp);
+        vector<int> dp(n + 1, 0);
+
+        for (int index = n - 1; index >= 0; index--) {
+            int cost = INT_MAX;
+
+            for (int i = 0; i < 3; i++) {
+                int next = index + i + 1;
+
+                if (next <= n) {
+                    cost = min(cost, dp[next] + costs[next - 1] + (i + 1) * (i + 1));
+                }
+            }
+
+            dp[index] = cost;
+        }
+
+        return dp[0];
     }
 };
