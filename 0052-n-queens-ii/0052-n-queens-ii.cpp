84b55fa5b874e24c95adb64d7354b7c3faa768b4
Harshal Savaliya <88780621+hetx19@users.noreply.github.com>
2026-08-17 05:39:42 +0530

    Time: 3 ms (40.21%), Space: 8.6 MB (31.1%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+private:
+    void solve(int n, int col,vector<bool>& leftRow, vector<bool>& upperDiagonal, vector<bool>& lowerDiagonal, vector<string>& board, int& ans) {
+        if (col == n) {
+            ans++;
+            return;
+        }
+
+        for (int row = 0; row < n; row++) {
+            if (!leftRow[row] && !upperDiagonal[n - 1 + col - row] && !lowerDiagonal[row + col]) {
+                board[row][col] = 'Q';
+                leftRow[row] = true;
+                upperDiagonal[n - 1 + col - row] = true;
+                lowerDiagonal[row + col] = true;
+
+                solve(n, col + 1, leftRow, upperDiagonal, lowerDiagonal, board, ans);
+
+                board[row][col] = '.';
+                leftRow[row] = false;
+                upperDiagonal[n - 1 + col - row] = false;
+                lowerDiagonal[row + col] = false;
+            }
+        }
+    }
+
+public:
+    int totalNQueens(int n) {
+        int ans = 0;
+        vector<string> board(n);
+
+        string s(n, '.');
+        for (int i = 0; i < n; i++) {
+            board[i] = s;
+        }
+
+        vector<bool> leftRow(n, false), upperDiagonal(2 * n - 1, false), lowerDiagonal(2 * n - 1, false);
+
+        solve(n, 0, leftRow, upperDiagonal, lowerDiagonal, board, ans);
+
+        return ans;
+    }
+};
