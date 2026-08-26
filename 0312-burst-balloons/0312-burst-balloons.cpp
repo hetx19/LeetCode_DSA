5b9286eed49aa58a73ca0392b369afa9cab34c5c
Harshal Savaliya <88780621+hetx19@users.noreply.github.com>
2026-08-26 11:26:34 +0530

    Time: 91 ms (87.34%), Space: 14 MB (7.12%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,33 +1,30 @@
 class Solution {
-private:
-    int solve(vector<int>& nums, int start, int end, vector<vector<int>>& dp) {
-        if (start > end) {
-            return 0;
-        }
-
-        if (dp[start][end] != -1) {
-            return dp[start][end];
-        }
-
-        int maxCoins = INT_MIN;
-
-        for (int index = start; index <= end; index++) {
-            int coins = nums[start - 1] * nums[index] * nums[end + 1] + solve(nums, start, index - 1, dp) + solve(nums, index + 1, end, dp);
-
-            maxCoins = max(maxCoins, coins);
-        }
-
-        return dp[start][end] = maxCoins;
-    }
-
 public:
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
         nums.insert(nums.begin(), 1);
         nums.push_back(1);
 
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
 
-        return solve(nums, 1, n, dp);
+        for (int start = n; start > 0; start--) {
+            for (int end = 1; end <= n; end++) {
+                if (start > end) {
+                    continue;
+                }
+
+                int maxCoins = INT_MIN;
+                
+                for (int index = start; index <= end; index++) {
+                    int coins = nums[start - 1] * nums[index] * nums[end + 1] + dp[start][index - 1] + dp[index + 1][end];
+
+                    maxCoins = max(maxCoins, coins);
+                }
+                
+                dp[start][end] = maxCoins;
+            }
+        }
+
+        return dp[1][n];
     }
 };
